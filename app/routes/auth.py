382dbc89a60271f24783e08cd60a5681a78a7faa
bachusaketh <sakethbachu8@gmail.com>
2026-03-29 09:50:53 +0530

    Added login API with JWT

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
+from app.dependencies.auth import get_current_user
 from app.models.user import User
-from app.schemas.user_schema import UserCreate
+from app.schemas.user_schema import UserCreate, UserLogin
 from app.core.security import hash_password
+from app.core.security import verify_password
+from app.utils.token import create_access_token
 
 router = APIRouter()
 
@@ -31,3 +34,42 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return {"message": "User created successfully"}
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        return {"error": "Invalid email or password"}
+
+    if not verify_password(user.password, db_user.password):
+        return {"error": "Invalid email or password"}
+
+    token = create_access_token({
+        "sub": db_user.id,
+        "role": db_user.role
+    })
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
+
+from app.dependencies.auth import get_current_user
+
+@router.get("/profile")
+def get_profile(current_user: dict = Depends(get_current_user)):
+    return {
+        "message": "Access granted",
+        "user": current_user
+    }
+
+from app.dependencies.auth import require_admin
+
+@router.get("/admin")
+def admin_route(current_user: dict = Depends(require_admin)):
+    return {
+        "message": "Welcome Admin",
+        "user": current_user
+    }
